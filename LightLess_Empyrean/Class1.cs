375c1af4a05a32af319e08e4a5c583e15352d73c
AludisNHG <aludisnhg@hotmail.com>
2024-04-30 14:56:41 -0300

    added stuff

---
+++ LightLess_Empyrean/Class1.cs
@@ -0,0 +1,24 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+
+namespace LightLess_Empyrean;
+
+public class BiomeWorker_LightlessEmpyrean : BiomeWorker
+{
+	public override float GetScore(Tile tile, int tileID)
+	{
+		if (tile.WaterCovered)
+		{
+			return -100f;
+		}
+		if (tile.temperature < -15f || tile.temperature > 20f)
+		{
+			return 0f;
+		}
+		if (tile.rainfall < 1300 || tile.rainfall > 1500)
+		{
+			return 0f;
+		}
+		return 15f + (tile.temperature - 6f) + (tile.rainfall - 570) / 180f;
+	}
+}
